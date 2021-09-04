6127dd23e290e3d7ac302b617929d26dc9aca489
Monica <monicasayama@gmail.com>
2021-09-04 00:03:00 -0300

    listAllUsers: add controller

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    let { user_id } = request.headers;
+    user_id = Array.isArray(user_id) ? user_id[0] : user_id;
+    try {
+      const listUsers = this.listAllUsersUseCase.execute({ user_id });
+      return response.status(200).send(listUsers);
+    } catch (error) {
+      let code: number;
+      if (error.message === "Usuário sem permissão") {
+        code = 401;
+      }
+      code = 400;
+      return response.status(code).json({ error: error.message });
+    }
   }
 }
 
